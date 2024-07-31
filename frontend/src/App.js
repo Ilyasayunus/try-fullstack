bbf3cca40583c11f6eb79a33ac9904dff454f38d
Ilyasayunus <yunusilyasa@gmail.com>
2024-07-31 22:14:05 +0700

    complete component in frontend & finish the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Userlist from "./components/Userlist";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
 
 function App() {
   return (
-    <div>
-      <Userlist></Userlist>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Userlist />} />
+        <Route path="add" element={<AddUser />} />
+        <Route path="edit/:id" element={<EditUser />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- frontend/src/components/Userlist.js
+++ frontend/src/components/Userlist.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
-export const Userlist = () => {
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get('http://localhost:5000/users');
+    setUsers(response.data);
+  };
+
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/users/${id}`);
+      getUsers();
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   return (
-    <div className="columns mt-5">
-        <div className="column is-half">
-            <h1>Lorem ipsum dolor sit amet.</h1>
-        </div>
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <Link to={`add`} className='button is-success'>Add New</Link>
+        <table className="table is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <Link to={`edit/${user.id}`}className='button is-small is-info'>Edit</Link>
+                  <button onClick={()=> deleteUser(user.id)} className='button is-small is-danger'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 };
 
-export default Userlist;
+export default UserList;
